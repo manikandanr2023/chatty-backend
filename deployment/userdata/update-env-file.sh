51cc960f8d655ddcc0694d042fdd3a79adbe1fbf
manikandanr2023 <manikandanengg2023@gmail.com>
2025-03-02 21:32:12 +0530

    setup circleci configuration

--- deployment/userdata/update-env-file.sh
+++ deployment/userdata/update-env-file.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
-yum update -y
-yum install -y zip
 echo "update env file"
+
 aws s3 sync s3://chattyapp-env-file/develop/ .
 unzip env-file.zip
 cp .env.develop .env
 rm .env.develop
-sed -i -e "s|\(^REDIS_HOST=\).*|REDIS_HOST=redis://$ELASTICACHE_ENDPOINT:6379|g" .env
+
+sed -i "s|\(^REDIS_HOST=\).*|REDIS_HOST=redis://$ELASTICACHE_ENDPOINT:6379|g" .env
+
 rm -rf env-file.zip
 cp .env .env.develop
-zip env-file.zip .env.develop
+zip -r env-file.zip .env.develop
+
 aws --region us-east-1 s3 cp env-file.zip s3://chattyapp-env-file/develop/
+
 rm -rf .env*
 rm -rf env-file.zip
-
