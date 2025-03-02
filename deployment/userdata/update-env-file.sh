126b8ed770e451c1e7823c0777d2a24fa9c48684
manikandanr2023 <manikandanengg2023@gmail.com>
2025-03-02 23:20:56 +0530

    setup circleci configuration

--- deployment/userdata/update-env-file.sh
+++ deployment/userdata/update-env-file.sh
@@ -1,26 +1,18 @@
 #!/bin/sh
 echo "update env file"
+
 aws s3 sync s3://chattyapp-env-file/develop/ .
 unzip env-file.zip
 cp .env.develop .env
 rm .env.develop
-sed -i -e "s|\(^REDIS_HOST=\).*|REDIS_HOST=redis://$ELASTICACHE_ENDPOINT:6379|g" .env
+
+sed -i "s|\(^REDIS_HOST=\).*|REDIS_HOST=redis://$ELASTICACHE_ENDPOINT:6379|g" .env
+
 rm -rf env-file.zip
 cp .env .env.develop
+tar -czf env-file.tar.gz .env.develop  # Use tar instead of zip
 
-# Variables
-SOURCE_FILE_OR_DIR=".env.develop"
-ARCHIVE_NAME="env-file.zip"
+aws --region us-east-1 s3 cp env-file.tar.gz s3://chattyapp-env-file/develop/
 
-# Compress using zip
-if zip -r "$ARCHIVE_NAME" "$SOURCE_FILE_OR_DIR"; then
-    echo "Compression successful: $ARCHIVE_NAME"
-else
-    echo "Compression failed!"
-    exit 1
-fi
-
-
-aws --region us-east-1 s3 cp env-file.zip s3://chattyapp-env-file/develop/
 rm -rf .env*
-rm -rf env-file.zip
+rm -rf env-file.tar.gz
