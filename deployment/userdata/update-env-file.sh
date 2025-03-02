2b52e2a51c01aee7510b7669fdbb3de74e791d35
manikandanr2023 <manikandanengg2023@gmail.com>
2025-03-02 22:46:28 +0530

    setup circleci configuration

--- deployment/userdata/update-env-file.sh
+++ deployment/userdata/update-env-file.sh
@@ -1,19 +1,26 @@
 #!/bin/sh
 echo "update env file"
-echo zip --version
 aws s3 sync s3://chattyapp-env-file/develop/ .
 unzip env-file.zip
 cp .env.develop .env
 rm .env.develop
-
-sed -i "s|\(^REDIS_HOST=\).*|REDIS_HOST=redis://$ELASTICACHE_ENDPOINT:6379|g" .env
-
+sed -i -e "s|\(^REDIS_HOST=\).*|REDIS_HOST=redis://$ELASTICACHE_ENDPOINT:6379|g" .env
 rm -rf env-file.zip
 cp .env .env.develop
-zip -r env-file.zip .env.develop
+
+# Variables
+SOURCE_FILE_OR_DIR=".env.develop"
+ARCHIVE_NAME="env-file.zip"
+
+# Compress using zip
+if zip -r "$ARCHIVE_NAME" "$SOURCE_FILE_OR_DIR"; then
+    echo "Compression successful: $ARCHIVE_NAME"
+else
+    echo "Compression failed!"
+    exit 1
+fi
+
 
 aws --region us-east-1 s3 cp env-file.zip s3://chattyapp-env-file/develop/
-
 rm -rf .env*
 rm -rf env-file.zip
-echo zip --version
