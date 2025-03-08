6c71b5ecd6accd46edabc0a076d3665971b333d0
manikandanr2023 <manikandanengg2023@gmail.com>
2025-03-08 22:52:51 +0530

    setup circleci configuration

--- deployment/userdata/delete-asg.sh
+++ deployment/userdata/delete-asg.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-
-
 ASG=$(aws autoscaling describe-auto-scaling-groups --no-paginate --output text \
     --query "AutoScalingGroups[?Tags[?Key=='Type' && Value=='$ENV_TYPE']].AutoScalingGroupName")
 
