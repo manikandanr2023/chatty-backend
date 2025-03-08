74835d2e8ea1c549dadc070c73cfb324ce0cc20f
manikandanr2023 <manikandanengg2023@gmail.com>
2025-03-08 19:43:44 +0530

    setup circleci configuration

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 cd /home/ec2-user/chatty-backend
-sudo rm -rf env-file.zip
-sudo rm -rf .env
-sudo rm -rf .env.develop
+sudo rm -rf .env .env.develop env-file.tar.gz env-file.zip
 aws s3 sync s3://chattyapp-env-file/develop/ .
-sudo -u root unzip env-file.zip
+sudo -u root tar -xzf env-file.tar.gz
 sudo chmod 644 .env.develop
 sudo cp .env.develop .env
 sudo pm2 delete all
